e1c16daa737ee06d6144bbf98a2ebcf0c49608cb
wannomiya <zgmfkx10.kay@gmail.com>
2020-07-01 10:42:40 +0900

    udetail.htmlの処理を追加

--- CleanerSchedule/src/main/java/com/example/demo/schedule/domain/service/PlanService.java
+++ CleanerSchedule/src/main/java/com/example/demo/schedule/domain/service/PlanService.java
@@ -18,7 +18,26 @@ public class PlanService {
 	public List<Plan> findAll() {
 		return planRepository.findAll();
 	}
+	// 1件取得
+	public Plan findOne(int planId) {
 
+		return planRepository.findOne(planId);
+	}
+
+	//1件更新
+//	public boolean updateFalse(Plan plan) {
+//
+//		// 判定用変数
+//		boolean result = false;
+//
+//		// １件更新
+//		int rowNumber = planRepository.updateOne(plan);
+//		if (rowNumber > 0) {
+//			// update成功
+//			result = true;
+//		}
+//		return result;
+//	}
 
 	//休み申請者取得
 
